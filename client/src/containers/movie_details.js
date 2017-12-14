6e690370dc3314dd40323e10a1e09b518dddcd54
codingkapoor <mailtoshivamk@gmail.com>
2017-12-14 18:36:52 +0530

    refactored movieDetails

--- client/src/containers/movie_details.js
+++ client/src/containers/movie_details.js
@@ -5,26 +5,68 @@ import { getMovieById } from '../actions';
 
 class MovieDetails extends Component {
     componentDidMount() {
-        this.props.getMovieById('569190ca24de1e0ce2dfcd4f');
+        const { id } = this.props.match.params;
+        this.props.getMovieById(id);
     }
 
     render() {
-        if(this.props.movie) {
-            return (
-                <div>
-                    Page for movie details! Id: ${ this.props.movie[0]._id }
-                </div>
-            );
-        } else {
+        if(!this.props.movie)
             return <div></div>;
-        }
+
+        const movie = this.props.movie;
+        return (
+            <ul>
+                <img src={ movie.poster } />
+                <li>
+                    <b>Title:</b> { movie.title }
+                </li>
+                <li>
+                    <b>Year:</b> { movie.year }
+                </li>
+                <li>
+                    <b>Rated:</b> { movie.rated }
+                </li>
+                <li>
+                    <b>Released:</b> { movie.released }
+                </li>
+                <li>
+                    <b>Runtime:</b> { movie.runtime }
+                </li>
+                <li>
+                    <b>Genres:</b> { movie.genres.toString() }
+                </li>
+                <li>
+                    <b>Director:</b> { movie.director }
+                </li>
+                <li>
+                    <b>Writers:</b> { movie.writers.toString() }
+                </li>
+                <li>
+                    <b>Actors:</b> { movie.actors.toString() }
+                </li>
+                <li>
+                    <b>Plot:</b> { movie.plot }
+                </li>
+                <li>
+                    <b>IMDB:</b> { movie.imdb.rating }, { movie.imdb.votes }
+                </li>
+                <li>
+                    <b>Metacritic:</b> { movie.metacritic }
+                </li>
+                <li>
+                    <b>Awards:</b> { movie.awards.text }
+                </li>
+            </ul>
+        );
     }
 }
 
 function mapStateToProps(state) {
-    return {
-        movie: state.moviesPerPage.movie,
-    };
+    let movie = state.moviesPerPage.movie;
+    if(state.moviesPerPage.movie)
+        movie = movie[0];
+
+    return { movie };
 }
 
 export default connect(mapStateToProps, { getMovieById })(MovieDetails);
